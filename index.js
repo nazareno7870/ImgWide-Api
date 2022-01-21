cd282447b3837155cd31cb3a009b13ce9d8d3d97
Nazareno <nazareno7870@gmail.com>
2022-01-21 15:38:25 -0300

    Added Dist

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const app = express();
 const handleErrors = require('./middleware/handleErrors.js');
 const postsRouter = require('./controllers/Posts');
 const tagsRouter = require('./controllers/Tags');
-const path = require('path');
 
 app.use(cors())
 app.use(express.json())
 
 
-app.use('api/users',usersRouter)
-app.use('api/posts',postsRouter)
-app.use('api/tags',tagsRouter)
-app.use('/',express.static('../app/dist'))
+app.use('/api/users/',usersRouter)
+app.use('/api/posts/',postsRouter)
+app.use('/api/tags/',tagsRouter)
+app.use('/',express.static('/dist'))
 app.use('/imgs',express.static('imgs'))
 
 
